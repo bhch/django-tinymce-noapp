86fe3b29dd04eb5ab678215cbe6ca3df9c388aa5
Bharat Chauhan <brattcee@yahoo.com>
2014-04-21 17:01:38 +0530

    Update tinymce_4_config.js

--- tinymce_config_js/tinymce_4_config.js
+++ tinymce_config_js/tinymce_4_config.js
@@ -1,1 +1,30 @@
-new 4
+// TinyMCE 4 configuration
+// Modify the following code to customize TinyMCE
+
+tinymce.init({
+	selector: "textarea",
+	// Only required plugins are included. However, you can add more as per your needs.
+	// Spellchecker plugin is excluded as it produces an error in version 4.x
+	plugins: [
+			"advlist autolink link image lists charmap preview hr",
+			"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media",
+			"table contextmenu emoticons textcolor"
+	],
+
+	// Customize the toolbars below. You can also add new ones.
+	toolbar1: "bold italic underline strikethrough | blockquote | bullist numlist | subscript superscript | table | hr | formatselect fontselect fontsizeselect",
+	toolbar2: "undo redo | forecolor backcolor | link unlink | image media | alignleft aligncenter alignright alignjustify | outdent indent | charmap emoticons | searchreplace | removeformat code | preview fullscreen",
+	// toolbar3: " add buttons here ",
+
+	// Aditional options. Customize them as per your needs.
+	height: 350,
+	resize: "both",
+	image_advtab: true,
+	toolbar_items_size: "medium",
+	menubar: false,
+	
+	// Example content CSS (should be your site CSS) for better typography
+	content_css : "/static/css/style.css"
+	// If your stylesheet is inside `static/css/` directory, just replace
+	// `style.css` with your stylesheet's name. You don't need to change the path.
+});
